d9aae80cbc404a1b79154c2c951a4e4168d002ed
bilalbbh106-hash <bilalbbh106@gmail.com>
2025-11-25 20:51:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 # === config.py ===
 
-TELEGRAM_BOT_TOKEN = "7614271148:AAFbb5m7S9qFVxu7rgNw1q2v0Clw10yp9KE"
+TELEGRAM_BOT_TOKEN = "76142"
 ADMIN_ID = 7784082779
 
 # Binance
-BINANCE_API_KEY = "3KVTGxCD6OEuMGtKCmaoKxWkzJpnuDSXkEAIOYZGk1vuK4UfzzxRzliSi3loaw4a"
-BINANCE_SECRET = "MU7Roc8HGlQs1RUCLgigUEs83Sx9s7kG9ldhSMxD0YiQDEadf2hy77YdgxD4782f"
+BINANCE_API_KEY = ""
+BINANCE_SECRET = ""
 
 # Payeer
 PAYEER_API_KEY = "PUT_PAYEER_KEY"
 PAYEER_ACCOUNT = "P1135268346"
 
 # Database
-MONGO_URI = "mongodb+srv://bilalbbh106_db_user:uCAgC7JY4YXLn6vT@cluster0.mq8hbfh.mongodb.net/?appName=Cluster0"
+MONGO_URI = "mongodb+srv://:/?appName=Cluster0"
 DB_NAME = "bilalbbh106_db_user"
